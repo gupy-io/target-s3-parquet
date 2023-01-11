6c586ca4be72377e63c2fc42bd8e037b3e90e957
lorransr <lorran.rodr@gmail.com>
2023-01-11 11:00:11 -0300

    fix: github ci

--- target_s3_parquet/target.py
+++ target_s3_parquet/target.py
@@ -24,7 +24,7 @@ class TargetS3Parquet(Target):
             required=True,
         ),
         th.Property("athena_database", th.StringType, required=True),
-        th.Property("add_record_metadata", th.BooleanType, default=False),
-        th.Property("stringify_schema", th.BooleanType, default=False),
+        th.Property("add_record_metadata", th.BooleanType, default=None),
+        th.Property("stringify_schema", th.BooleanType, default=None),
     ).to_dict()
     default_sink_class = S3ParquetSink
